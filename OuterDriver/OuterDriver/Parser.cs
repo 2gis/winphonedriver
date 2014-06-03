505c9fe42236d0a0658f87cea4034410b8181062
Nick Abalov <n.abalov@2gis.ru>
2014-06-03 11:47:28 +0700

    Parser clean up
    code clean up
    Removed some duplicate code, still needs more work

--- OuterDriver/OuterDriver/Parser.cs
+++ OuterDriver/OuterDriver/Parser.cs
@@ -3,62 +3,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace OuterDriver {
+namespace OuterDriver
+{
 
-    class Parser {
+    class Parser
+    {
 
-        private static List<String> commandsToProxy = new List<String> { "element", "elements", "text", "displayed", "location" };
-        private static List<String> commandsWithGET = new List<String> { "text", "displayed", "location" };
+        private static readonly List<String> CommandsToProxy = new List<String> { "element", "elements", "text", "displayed", "location" };
+        private static readonly List<String> CommandsWithGet = new List<String> { "text", "displayed", "location" };
 
-        public static String GetRequestUrn(String request) {
-            String[] firstHeaderTokens = request.Split(' ');
+        public static String GetRequestUrn(String request)
+        {
+            var firstHeaderTokens = request.Split(' ');
             return firstHeaderTokens[1];
         }
 
-        public static String GetLastToken(String urn) {
-            String[] urnTokens = urn.Split(new String[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
-            String command = urnTokens[urnTokens.Length - 1];
+        public static String GetLastToken(String urn)
+        {
+            var urnTokens = SplitTokens(urn);
+            var command = urnTokens[urnTokens.Length - 1];
             return command;
         }
 
-        public static String GetRequestCommand(String request) {
-            String[] requestTokens = request.Split(' ');
-            String urn = requestTokens[1];
-            //take care of the slash in the end of the string
-            String[] urnTokens = urn.Split(new String[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
-            String command = urnTokens[urnTokens.Length - 1];
+        public static String GetRequestCommand(String request)
+        {
+            var tokens = GetUrnTokens(request);
+            var command = tokens[tokens.Length - 1];
             return command;
         }
 
         //decides if the request should be simply proxied by looking at the last command token
-        public static bool ShouldProxy(String request) {
-            String urn = GetRequestUrn(request);
-            return commandsToProxy.Contains(GetLastToken(urn));
+        public static bool ShouldProxy(String request)
+        {
+            var urn = GetRequestUrn(request);
+            return CommandsToProxy.Contains(GetLastToken(urn));
         }
 
         //deserializes json string array and returns a single String
-        public static String GetKeysString(String requestContent) {
-            String result = String.Empty;
-            JsonKeysContent jsonContent = JsonConvert.DeserializeObject<JsonKeysContent>(requestContent);
-            String[] value = jsonContent.GetValue();
-            foreach (String str in value) {
-                result += str;
-            }
-            return result;
+        public static String GetKeysString(String requestContent)
+        {
+            var result = String.Empty;
+            var jsonContent = JsonConvert.DeserializeObject<JsonKeysContent>(requestContent);
+            var value = jsonContent.GetValue();
+            return value.Aggregate(result, (current, str) => current + str);
         }
 
-        public static int GetRequestLength(String request) {
-            String[] requestTokens = request.Split(' ');
-            String urn = requestTokens[1];
-            String[] urnTokens = urn.Split(new String[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
-            return urnTokens.Length;
+        public static int GetRequestLength(String request)
+        {
+            return GetUrnTokens(request).Length;
+        }
+
+        private static String[] SplitTokens(String urn)
+        {
+            var urnTokens = urn.Split(new[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+            return urnTokens;
+        }
+
+        public static String[] GetUrnTokens(String request)
+        {
+            var urn = GetRequestUrn(request);
+            return SplitTokens(urn);
         }
 
         //chooses the request method by looking at the last command token
-        public static String ChooseRequestMethod(String uri) {
-            if (commandsWithGET.Contains(GetLastToken(uri)))
-                return "GET";
-            return "POST";
+        public static String ChooseRequestMethod(String uri)
+        {
+            return CommandsWithGet.Contains(GetLastToken(uri)) ? "GET" : "POST";
         }
     }
 }
