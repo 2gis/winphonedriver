cacb72bdb3bc391150160d75c544aa3930196da4
Nick Abalov <n.abalov@2gis.ru>
2014-06-20 16:18:26 +0700

    Preparations for 8.1

---
+++ OuterDriver/OuterDriver/EmulatorHelpers/IDeployer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OuterDriver.EmulatorHelpers
+{
+    interface IDeployer
+    {
+
+        string DeviceName { get; }
+
+        void Deploy(string appPath, String appIdString, int launchDelay = 3500);
+
+        String ReceiveIpAddress();
+
+        void Disconnect();
+    }
+}
