49e4523544dcfd1e7e687f7551de4a5be277f5a9
Nick Abalov <n.abalov@2Gis.ru>
2014-10-28 16:27:22 +0600

    Change location determination logic for click and gestures
    Click and start gesture from center of "in-view" only element area

--- WindowsPhoneDriver/WindowsPhoneDriver.InnerDriver/Commands/FrameworkElementExtensions.cs
+++ WindowsPhoneDriver/WindowsPhoneDriver.InnerDriver/Commands/FrameworkElementExtensions.cs
@@ -27,6 +27,17 @@
             return center;
         }
 
+        internal static Point GetCoordinatesInView(this FrameworkElement element, UIElement visualRoot)
+        {
+            var point = element.TransformToVisual(visualRoot).Transform(new Point(0, 0));
+            var center = new Point(point.X + (int)(element.ActualWidth / 2), point.Y + (int)(element.ActualHeight / 2));
+            var bounds = new Rect(point, new Size(element.ActualWidth, element.ActualHeight));
+            var boundsInView = new Rect(new Point(0, 0), visualRoot.RenderSize);
+            boundsInView.Intersect(bounds);
+
+            return boundsInView.IsEmpty ? center : new Point(boundsInView.X + (int)(boundsInView.Width / 2), boundsInView.Y + (int)(boundsInView.Height / 2));
+        }
+
         internal static string GetText(this FrameworkElement element)
         {
             var propertyNames = new List<string> { "Text", "Content" };
